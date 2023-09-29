1275475a8dfb5fc26ec5fa754eb58509e8af5f64
Sarah Martel <57870970+sarahmarie23@users.noreply.github.com>
2023-09-28 23:41:46 -0700

    Add files via upload

---
+++ Lab1/Lab1Starter.java
@@ -0,0 +1,27 @@
+public class Lab1Starter {
+    public static void main(String[] args) {
+       // Welcome message
+        System.out.println("Welcome to the Coffee Shop");
+
+        // Coffee order details
+        String coffeeType = "Espresso";
+        int sugarSpoons = 2;
+        int creamSpoons = 1;
+        int cupsOrdered = 3;
+
+        // Display coffee recipe
+        System.out.println("Your " + coffeeType + " is ready!');
+        System.out.println("Sugar: " + sugarSpoons + " spoons");
+        System.out.println("Cream: " + creamSpoons + " spoons");
+
+        // Calculate the total price of the coffee
+        double pricePerCup = 2.5;
+        double totalPrice = cups * pricePerCup;  
+
+        // Display the total price
+        System.out.println("Total Price: $" + (totlPrice);
+
+        ## Closing message
+        System.out.println("Enjoy your coffee!")
+    }
+}
